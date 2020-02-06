ed832b7231587a922de6161f479ca61468e604b2
Karthick Kumar <karthickkumar678@gmail.com>
2020-02-06 22:59:59 +0530

    initial commit

---
+++ src/test/java/com/iq/hospital/HospitalManagementTests.java
@@ -0,0 +1,13 @@
+package com.iq.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
